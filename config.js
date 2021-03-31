27919c60745a5b6ac85acfe2537d7bed30997586
sunskwn <81666157+sunskwn@users.noreply.github.com>
2021-03-31 11:40:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-/*
- * @Author: PlanC
- * @Date: 2021-01-12 12:33:49
- * @LastEditTime: 2021-01-20 10:21:49
- * @FilePath: \iplanc.github.io\config.js
- */
+var lover = "魏娜";
 
-var lover = "雨雨";
-
-var start = new Date("2020-11-11 22:30:00"); //2020-11-11 22:30:00
-//var end = new Date("9999-12-31"); //9999-12-31
+var start = new Date("2018-4-20 00:00:00"); //2020-11-11 22:30:00
+var end = new Date("2021-3-31"); //9999-12-31
